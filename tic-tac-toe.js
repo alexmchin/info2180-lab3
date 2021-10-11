e8093a3c85e856fcff8a86acfe9b577b63677217
alexmchin <alexmchin12@gmail.com>
2021-10-10 21:06:57 -0500

    Alternates placing an X or an O in grid spaces

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,21 +1,70 @@
 
 window.onload = function(){
     var id = 0;
-    var x = true;
+    var gameActive = true;
+    //var currentPlayer = "X";
     var status = document.getElementById("status");
     var board = document.getElementById("board");
     var boardList = board.querySelectorAll("div");
     var button = document.querySelector("button");
+    let currentPlayer = "X";
+    let gameState = ["", "", "", "", "", "", "", "", ""];
+
+    /*
+    const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+    //status.innerHTML = currentPlayerTurn();
 
 
+    function handleCellPlayed(clickedCell, clickedCellIndex) {
+        gameState[clickedCellIndex] = currentPlayer;
+        clickedCell.innerHTML = currentPlayer;
+    }
+
+    function handlePlayerChange() {
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        status.innerHTML = currentPlayerTurn();
+    }
+
+    function handleCellClick(clickedCellEvent) {
+        const clickedCell = clickedCellEvent.target;
+        const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+    
+        if (gameState[clickedCellIndex] !== "" || !gameActive) {
+            return;
+        }
+    
+        console.log(boardList);
+        //handleCellPlayed(clickedCell, clickedCellIndex);
+        //handleResultValidation();
+    }
+    */
 
 
-    boardList.forEach( element => {
-        element.setAttribute("class", "square");
-        //element.setAttribute("id", id);
-        //id++;
+    boardList.forEach( space => {
+        space.setAttribute("class", "square");
+        space.setAttribute("id", id);
+        id++;
 
         console.log(boardList);
+
+
+        space.addEventListener("click", clicked =>{
+
+            if(currentPlayer == "X" && !space.classList.contains("X") && !space.classList.contains("O")){
+                space.textContent = currentPlayer;
+                space.classList.add(currentPlayer);
+                currentPlayer= "O";
+            }
+
+            if(currentPlayer == "O" && !space.classList.contains("X") && !space.classList.contains("O")){
+                space.textContent = currentPlayer;
+                space.classList.add(currentPlayer);
+                currentPlayer= "X";
+            }
+        });
+
     });
 
+    
 };
